f83fa10e0b5d6e962cf9dfe179d47fcc40f0279b
ahlvin <42684180+ahlvin@users.noreply.github.com>
2020-12-26 22:05:19 -0500

    Lec 83

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/CrudService.java
@@ -1,16 +1,17 @@
 package guru.springframework.sfgpetclinic.services;
 
+
+import guru.springframework.sfgpetclinic.model.Pet;
+
 import java.util.Set;
 
 public interface CrudService<T, ID> {
 
     Set<T> findAll();
-
     T findById(ID id);
-
-    T save(T object);
-
+    Pet save(T object);
     void delete(T object);
+    void deleteById(ID id);
 
-    void deleteAll(T object);
+
 }
