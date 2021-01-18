4e7f25e28def1acc28dca5992878d1047910531a
ahlvin <42684180+ahlvin@users.noreply.github.com>
2021-01-17 22:13:17 -0500

    Created Visit Map Service
    Closes #34

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+
+/**
+ * Created by vineet on Jan, 2021
+ */
+public interface VisitService extends CrudService<Visit, Long>{
+}
